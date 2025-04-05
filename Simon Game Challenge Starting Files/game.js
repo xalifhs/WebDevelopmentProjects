793dc7f494447db3f593755a3bc5f0bd370521f4
FillTso <97851822+xalifhs@users.noreply.github.com>
2025-04-05 20:10:25 +0300

    got the simon game done
    7-8-9 were tough butDONE!!!

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -8,32 +8,50 @@ var gamePattern = [];
 //click pattern
 var userClickedPattern =[];
 
-                                                                                                // started toggle variable
-                                                                                                var started = true;
+// started toggle variable
+var started = false;
+
+//level
+var level = 0;
+
+//start game
+$(document).keydown(function(){
+    
+  if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+  }
+});
                                                                                             
 // handler
-$(".btn").click(function handler(){
+$(".btn").click(function(){
     // the button id
-    var userChosenColour =$(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 })
 //the function that activates sound on click
 function playSound(name){
     var sound = new Audio("sounds/"+name+".mp3");
     sound.play();
 }
-                                                                                                        $(document).keydown(function(started){
-                                                                                                            
-                                                                                                            if (started = true) {
-                                                                                                                nextSequence();
-                                                                                                                started =false;
-                                                                                                            }
-                                                                                                        });
+
+
 
 //the next sequence function
 function nextSequence(){
+    //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+    userClickedPattern = [];
+    
+    level++
+
+    //5. Inside nextSequence(), update the h1 with this change in the value of level.
+    $("#level-title").text("Level " + level);
     
     //random number 0-3
     var randomNumber = Math.floor(Math.random()*4);
@@ -45,7 +63,7 @@ function nextSequence(){
     gamePattern.push(randomChosenColour);
     
     //flash
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(1);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     //sound
     playSound(randomChosenColour);
@@ -61,6 +79,44 @@ function animatePress(currentColour){
     setTimeout(function () {
       $("#"+currentColour).removeClass("pressed");
     }, 100);
+}
+
+function checkAnswer(currentLevel){
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        
+  
+        //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+        if (userClickedPattern.length === gamePattern.length){
+  
+          //5. Call nextSequence() after a 1000 millisecond delay.
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+  
+        }
+  
+      } else {
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+  
+      }
+
+      
+    
+}
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
 
 
